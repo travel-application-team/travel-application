c0dbeafc844c69b00462c5c9b55ad722b23e3a9d
eunseobb <seobseob0810@naver.com>
2025-09-08 19:12:10 +0900

    feat: 매일 자정 like.csv를 업데이트하도록 스케줄러 추가

--- src/main/java/travel/travelapplication/TravelApplication.java
+++ src/main/java/travel/travelapplication/TravelApplication.java
@@ -5,14 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableMongoRepositories
 @EnableMongoAuditing
+@EnableScheduling
 public class TravelApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TravelApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(TravelApplication.class, args);
+  }
 
 }
